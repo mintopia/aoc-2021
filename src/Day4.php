7b5eb4d23e59f7ea93c19badb1f530ce4fcd277e
Jessica Smith <jessica.smith@fasthosts.com>
2021-12-04 13:03:35 +0000

    Day 4 of AOC

--- src/Day4.php
+++ src/Day4.php
@@ -1,6 +1,7 @@
 <?php
 namespace Mintopia\Aoc2021;
 
+use Mintopia\Aoc2021\Helpers\Day4\Grid;
 use Mintopia\Aoc2021\Helpers\Result;
 
 class Day4 extends Day
@@ -8,13 +9,65 @@ class Day4 extends Day
     protected static $defaultName = 'aoc:day4';
     protected int $dayNumber = 4;
 
+    protected array $calledNumbers;
+    protected array $grids;
+
+    protected function loadData()
+    {
+        $data = $this->getArrayFromInputFile();
+        $this->calledNumbers = explode(',', $data[0]);
+        $count = 1;
+        for ($i = 1; $i + 5 <= count($data); $i += 5) {
+            $this->grids[] = new Grid($count, array_slice($data, $i, 5));
+            $count++;
+        }
+    }
+
+    protected function playWinningGame()
+    {
+        foreach ($this->calledNumbers as $number) {
+            foreach ($this->grids as $grid) {
+                $grid->mark($number);
+                if ($grid->isComplete()) {
+                    return $grid;
+                }
+            }
+        }
+    }
+
+    protected function playLosingGame()
+    {
+        $grids = $this->grids;
+        foreach ($this->calledNumbers as $number) {
+            foreach ($grids as $index => $grid) {
+                $grid->mark($number);
+                if ($grid->isComplete()) {
+                    if (count($grids) == 1) {
+                        return end($grids);
+                    }
+                    unset($grids[$index]);
+                }
+            }
+        }
+    }
+
     protected function part1(): Result
     {
-        return new Result(Result::PART1, 'foo');
+        $winningGrid = $this->playWinningGame();
+        if (!$winningGrid) {
+            throw new \Exception('Unable to find a winning grid');
+        }
+        $winningGrid->display($this->io);
+        return new Result(Result::PART1, $winningGrid->score());
     }
 
     protected function part2(Result $part1): Result
     {
-        return new Result(Result::PART2, 'bar');
+        $losingGrid = $this->playLosingGame();
+        if (!$losingGrid) {
+            throw new \Exception('Unable to find a losing grid');
+        }
+        $losingGrid->display($this->output);
+        return new Result(Result::PART2, $losingGrid->score());
     }
 }
---
+++ src/Helpers/Day4/Grid.php
@@ -0,0 +1,76 @@
+<?php
+namespace Mintopia\Aoc2021\Helpers\Day4;
+
+use Symfony\Component\Console\Helper\Table;
+use Symfony\Component\Console\Helper\TableSeparator;
+use Symfony\Component\Console\Output\OutputInterface;
+
+class Grid
+{
+    public array $rows = [];
+    public array $columns = [];
+    public int $number;
+
+    protected array $marked = [];
+
+    public function __construct(int $number, array $grid)
+    {
+        $this->number = $number;
+        $this->rows = [];
+        foreach ($grid as $gridRow) {
+            $this->rows[] = array_values(array_filter(explode(' ', $gridRow), function ($input) {
+                return $input !== '';
+            }));
+        }
+
+        $this->columns = array_map(null, ...$this->rows);
+    }
+
+    public function mark($number)
+    {
+        $this->marked[] = $number;
+    }
+
+    public function score()
+    {
+        $ourNumbers = array_merge(...$this->rows);
+        $notCalled = array_diff($ourNumbers, $this->marked);
+        $sum = array_sum($notCalled);
+        return $sum * end($this->marked);
+    }
+
+    public function isComplete()
+    {
+        foreach (array_merge($this->rows, $this->columns) as $row) {
+            $intersection = array_intersect($row, $this->marked);
+            if (count($intersection) == count($row)) {
+                return true;
+            }
+        }
+        return false;
+    }
+
+    public function display(OutputInterface $output)
+    {
+        $output->writeln([
+            "Grid {$this->number}",
+            ''
+        ]);
+        $table = new Table($output);
+        $lastIndex = count($this->rows) - 1;
+        foreach ($this->rows as $index => $row) {
+            $styledRow = [];
+            foreach ($row as $number) {
+                if (in_array($number, $this->marked)) {
+                    $number = "<fg=bright-green>{$number}</>";
+                }
+                $styledRow[] = $number;
+            }
+            $table->addRow($styledRow);
+            if ($index !== $lastIndex) {
+                $table->addRow(new TableSeparator());
+            }
+        }
+        $table->render();
+    }
+}
