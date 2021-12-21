788b1fab21ad4ceed61210ae595dfac5e44e5d4f
Jessica Smith <jess@mintopia.net>
2021-12-21 07:09:00 +0000

    Minor refactor

--- src/Day21.php
+++ src/Day21.php
@@ -9,6 +9,16 @@ class Day21 extends Day
 {
     protected string $key;
 
+    const DICE_FREQUENCIES = [
+        3 => 1,
+        4 => 3,
+        5 => 6,
+        6 => 7,
+        7 => 6,
+        8 => 3,
+        9 => 1,
+    ];
+
     protected function loadData(): void
     {
         $data = $this->getArrayFromInputFile();
@@ -45,6 +55,12 @@ class Day21 extends Day
 
     public function quantum(int $p1Space, int $p2Space, int $p1Score, int $p2Score, array &$outcomes = []): array
     {
+        // Check previous outcomes for this input
+        $key = "{$p1Space}-{$p2Space}-{$p1Score}-{$p2Score}";
+        if (isset($outcomes[$key])) {
+            return $outcomes[$key];
+        }
+
         // Win conditions
         if ($p1Score >= 21) {
             return [1, 0];
@@ -52,36 +68,22 @@ class Day21 extends Day
             return [0, 1];
         }
 
-        // Check previous outcomes for this input
-        $key = "{$p1Space}-{$p2Space}-{$p1Score}-{$p2Score}";
-        if (isset($outcomes[$key])) {
-            return $outcomes[$key];
-        }
-
-        $diceFrequencies = [
-            3 => 1,
-            4 => 3,
-            5 => 6,
-            6 => 7,
-            7 => 6,
-            8 => 3,
-            9 => 1,
-        ];
-
         $result = [0, 0];
-
         foreach (range(3, 9) as $diceRoll) {
             $space = $p1Space + $diceRoll;
             $space %= 10;
             $score = $p1Score + $space + 1;
 
+            // Repeat this but for player 2
             $p2Result = $this->quantum($p2Space, $space, $p2Score, $score, $outcomes);
-            $result[0] += $p2Result[1] * $diceFrequencies[$diceRoll];
-            $result[1] += $p2Result[0] * $diceFrequencies[$diceRoll];
+
+            // Take our result from Player 2 and add to our current result. Multiply by the number of ways we could have
+            // rolled this number.
+            $result[0] += $p2Result[1] * self::DICE_FREQUENCIES[$diceRoll];
+            $result[1] += $p2Result[0] * self::DICE_FREQUENCIES[$diceRoll];
         }
 
         // Store outcome for these inputs
-        $key = "{$p1Space}-{$p2Space}-{$p1Score}-{$p2Score}";
         $outcomes[$key] = $result;
         return $result;
     }
