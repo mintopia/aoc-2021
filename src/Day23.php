711c8b77cfa0a5ff33592dd8f954c28762e027b8
Jessica Smith <jessica.smith@fasthosts.com>
2021-12-23 14:55:42 +0000

    More WiP

--- src/Day23.php
+++ src/Day23.php
@@ -9,14 +9,39 @@ class Day23 extends Day
     protected function part1(): Result
     {
         $map = new Map($this->data);
-        $map->render($this->output);
-        echo $map->getState();
-        die();
-        return new Result(Result::PART1, 'foo');
+        $cost = $this->nextMove(0, $map);
+        return new Result(Result::PART1, $cost);
     }
 
     protected function part2(Result $part1): Result
     {
         return new Result(Result::PART2, 'bar');
+    }
+
+    function nextMove(int $costSoFar, Map $map, array &$states = [], array $movesSoFar = []) {
+        $state = $map->getState();
+        if (isset($states[$state])) {
+            return $state;
+        }
+        if ($map->isFinished()) {
+            echo "Found solution: {$costSoFar}\r\n";
+            return $costSoFar;
+        }
+
+        $moves = $map->getMoves();
+        $lowest = PHP_INT_MAX;
+        while (!$moves->isEmpty()) {
+            $move = $moves->extract();
+            if (in_array([$move->from, $move->to], $movesSoFar)) {
+                continue;
+            }
+            $thisMap = clone $map;
+            $thisMap->move($move->from, $move->to);
+            $cost = $costSoFar + $move->cost;
+            $totalCost = $this->nextMove($cost, $thisMap, $states, array_merge($movesSoFar, [[$move->from, $move->to]]));
+            $lowest = min($totalCost, $lowest);
+        }
+        $states[$state] = $lowest;
+        return $lowest;
     }
 }
--- src/Helpers/Day23/Map.php
+++ src/Helpers/Day23/Map.php
@@ -14,7 +14,29 @@ class Map
     ];
     protected array $hallway;
 
-    const HALLWAY_DOORS = [3, 5, 7, 9];
+    // Hallway is 1-11
+    // Rooms are A = 12, B = 13, C = 14, D = 15, A2 = 16
+
+    const HALLWAY_DOORS = [
+        2 => 'A',
+        4 => 'B',
+        6 => 'C',
+        8 => 'D',
+    ];
+
+    const ROOM_HALLWAY = [
+        'A' => 2,
+        'B' => 4,
+        'C' => 6,
+        'D' => 8,
+    ];
+
+    const ENERGY = [
+        'A' => 1,
+        'B' => 10,
+        'C' => 100,
+        'D' => 1000,
+    ];
 
     public function __construct(array $input)
     {
@@ -53,31 +75,120 @@ class Map
         return $state;
     }
 
-    public function getMoves()
+    public function getMoves(): \SplPriorityQueue
     {
+        $allMoves = new \SplPriorityQueue();
         // For each square in hallway and rooms, return possible moves + cost
-        foreach ($this->hallway as $hi => $square) {
+        foreach ($this->hallway as $hi => $amphipod) {
             $moves = [];
-            if ($square === 'x') {
+            if ($amphipod === 'x') {
                 // Empty square
                 continue;
             }
-            for ($i = $hi - 1; $i >= 0; $i--) {
-                if ($this->hallway[$i] != 'x') {
-                    // Occupied, we're done.
-                    break;
-                }
-                if (!in_array($i, self::HALLWAY_DOORS)) {
-                    $moves[] = $this->getCost($square) * ($hi - $i);
+            $moves = $this->getHallwayMoves($hi, $amphipod);
+            foreach ($moves as $to => $cost) {
+                $allMoves->insert((object) [
+                    'from' => $hi,
+                    'to' => $to,
+                    'cost' => $cost
+                ], -$cost);
+            }
+        }
+
+        foreach ($this->rooms as $letter => $room) {
+            foreach ($room as $ri => $amphipod) {
+                if ($amphipod === 'x') {
+                    // Empty Square
                     continue;
                 }
-                // Is this doorway our door and is it us
+                $door = self::ROOM_HALLWAY[$letter];
+                $moves = $this->getHallwayMoves($door, $amphipod);
+                $extraCost = $this->getCost($amphipod) * ($ri + 1);
+                foreach ($moves as $to => $cost) {
+                    $cost += $extraCost;
+                    $allMoves->insert((object) [
+                        'from' => $door . $ri,
+                        'to' => $to,
+                        'cost' => $cost,
+                    ], -$cost);
+                }
+                // No more in this room can move
+                break;
             }
-            // Check left or right for empty space
-            // If not doorway - OK
-            // If doorway and our doorway, recurse down
-                // If our letter and only our letter (or none) - OK
         }
+
+        return $allMoves;
+    }
+
+    public function move(int $from, int $to): void
+    {
+        if ($from <= 10) {
+            $value = $this->hallway[$from];
+            $this->hallway[$from] = 'x';
+        } else {
+            [$room, $position] = str_split((string) $from);
+            $value = $this->rooms[self::HALLWAY_DOORS[$room]][$position];
+            $this->rooms[self::HALLWAY_DOORS[$room]][$position] = 'x';
+        }
+
+        if ($to <= 10) {
+            $this->hallway[$to] = $value;
+        } else {
+            [$room, $position] = str_split((string) $to);
+            $this->rooms[self::HALLWAY_DOORS[$room]][$position] = $value;
+        }
+    }
+
+    protected function getHallwayMoves(int $hi, string $amphipod): array
+    {
+        $left = $this->getHallwayMovesDirection($hi, $amphipod, $hi - 1, -1, -1);
+        $right = $this->getHallwayMovesDirection($hi, $amphipod, $hi + 1, count($this->hallway), 1);
+        return $left + $right;
+    }
+
+    protected function getHallwayMovesDirection(int $hi, string $amphipod, int $from, int $to, int $adjustment): array
+    {
+        $moves = [];
+        $cost = $this->getCost($amphipod);
+        for ($i = $from; $i != $to; $i += $adjustment) {
+            if ($this->hallway[$i] != 'x') {
+                // Occupied, we're done.
+                break;
+            }
+            if (!array_key_exists($i, self::HALLWAY_DOORS)) {
+                $moves[$i] = $this->getCost($amphipod) * abs($hi - $i);
+                continue;
+            }
+            // Is this doorway our door and is it us
+            $door = self::HALLWAY_DOORS[$i];
+            if ($door != $amphipod) {
+                // It is not us, no valid moves here
+                continue;
+            }
+            // It is us! If it's empty or only us, then we can have it as a move!
+            $possible = [];
+            foreach ($this->rooms[$door] as $di => $square) {
+                $location = $i . $di;
+                if ($square == 'x') {
+                    $possible[$location] = $cost * $i * ($di + 1);
+                    continue;
+                }
+                if ($square != $amphipod) {
+                    $possible = [];
+                }
+                break;
+            }
+            $moves = $moves + $possible;
+        }
+        return $moves;
+    }
+
+    protected function getCost(string $amphipod): int
+    {
+        if (array_key_exists($amphipod, self::ENERGY)) {
+            return self::ENERGY[$amphipod];
+        }
+        return 0;
     }
 
     public function render(OutputInterface|ConsoleSectionOutput $output)
